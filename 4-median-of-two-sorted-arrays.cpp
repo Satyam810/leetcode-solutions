95d4f04203b4b427fd4a0658b0cfc6724c626d66
Satyam Kumar <152474324+Satyam810@users.noreply.github.com>
2026-07-09 23:40:57 +0530

    ✅ 4. Median of Two Sorted Arrays (Hard)

--- 4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays.cpp
@@ -1,6 +1,16 @@
-class Solution {
-public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
-    }
-};
+        while (low <= high) {
+            int mid1 = (low + high) >> 1; // Calculate mid index for nums1
+            int mid2 = left - mid1; // Calculate mid index for nums2
+        
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1) / 2; // Calculate the left partition size
+        int low = 0, high = n1;
+        // Ensure nums1 is the smaller array for simplicity
+        if (n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
+        int n1 = nums1.size(), n2 = nums2.size();
+        
+public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+class Solution {
