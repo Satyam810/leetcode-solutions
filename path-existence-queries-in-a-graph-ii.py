e85779027e9eb6320dddecb211122e18ccdca6f6
Satyam Kumar <152474324+Satyam810@users.noreply.github.com>
2026-07-10 23:53:09 +0530

    ✅ Path Existence Queries in a Graph II (Hard)

--- path-existence-queries-in-a-graph-ii.py
+++ path-existence-queries-in-a-graph-ii.py
@@ -1,16 +1,10 @@
-from collections import deque
-
-class Solution:
-    def shortestPath(self, n: int, nums: list[int], maxDiff: int, queries: list[list[int]]) -> list[int]:
-        graph = [[] for _ in range(n)]
-        for i in range(n):
-            for j in range(i+1, n):
-                if abs(nums[i] - nums[j]) <= maxDiff:
-                    graph[i].append(j)
-                    graph[j].append(i)
-
-        result = []
-        for u, v in queries:
-            queue = deque([(u, 0)])
-            visited = set([u])
-            while queue:
+class Solution(object):
+    def pathExistenceQueries(self, n, nums, maxDiff, queries):
+        """
+        :type n: int
+        :type nums: List[int]
+        :type maxDiff: int
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        
