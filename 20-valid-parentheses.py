00393ac23cb0c63ec823b593f1982d65583f01a1
Satyam Kumar <152474324+Satyam810@users.noreply.github.com>
2026-07-10 22:35:20 +0530

    ✅ 20. Valid Parentheses (Easy)

--- 20-valid-parentheses.py
+++ 20-valid-parentheses.py
@@ -1,16 +1,14 @@
-  # Create an empty stack to store the opening brackets
-  stack = []
-  
-  # Iterate through the input string
-  for char in s:
-    # If we encounter an opening bracket, push it onto the stack
-    if char in bracket_map.values():
-      stack.append(char)
-    # If we encounter a closing bracket, check if the stack is empty or if the top of the stack does not match the closing bracket
-    elif char in bracket_map.keys():
-      if not stack or stack.pop() != bracket_map[char]:
-        return False
-  
-  bracket_map = {')': '(', '}': '{', ']': '['}
-  # Create a dictionary to map closing brackets to their corresponding opening brackets
-def isValid(s):
+class Solution:
+    def isValid(self, s):
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        opening_brackets = set(['(', '{', '['])
+        stack = []
+        
+        for char in s:
+            if char in opening_brackets:
+                stack.append(char)
+            elif char in bracket_map.keys():
+                if not stack or stack.pop() != bracket_map[char]:
+                    return False
+        
+        return not stack
