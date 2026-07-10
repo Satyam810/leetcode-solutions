cb26475ad79886fb756b8bb33c0b694a41dffcea
Satyam Kumar <152474324+Satyam810@users.noreply.github.com>
2026-07-11 00:04:58 +0530

    ✅ 3534. Path Existence Queries in a Graph II (Hard)

--- 3534-path-existence-queries-in-a-graph-ii.py
+++ 3534-path-existence-queries-in-a-graph-ii.py
@@ -1,16 +1,15 @@
 class Solution(object):
     def pathExistenceQueries(self, n, nums, maxDiff, queries):
-        """
-        :type n: int
-        :type nums: List[int]
-        :type maxDiff: int
-        :type queries: List[List[int]]
-        :rtype: List[int]
-        """
-        graph = {}
+        graph = [[] for _ in range(n)]
         for i in range(n):
-            graph[i] = []
             for j in range(n):
                 if i != j and abs(nums[i] - nums[j]) <= maxDiff:
                     graph[i].append(j)
-
+        answer = []
+        for ui, vi in queries:
+            min_distance = float('inf')
+            for i in range(n):
+                if ui in graph[i] and vi in graph[i]:
+                    min_distance = min(min_distance, abs(i - ui) + abs(i - vi))
+            answer.append(min_distance if min_distance != float('inf') else -1)
+        return answer
