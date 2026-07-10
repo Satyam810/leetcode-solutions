8be09cc900647a40f43811749c9813ef5bf818de
Satyam Kumar <152474324+Satyam810@users.noreply.github.com>
2026-07-10 23:42:12 +0530

    ✅ 3534. Path Existence Queries in a Graph II (Hard)

--- 3534-path-existence-queries-in-a-graph-ii.py
+++ 3534-path-existence-queries-in-a-graph-ii.py
@@ -1,16 +1,16 @@
-class Solution:
-    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[int]:
-        new_nums = sorted(enumerate(nums), key=lambda x: x[1])
-        get_i = [0] * n
-        for i, (orig, _) in enumerate(new_nums):
-            get_i[orig] = i
+function constructGraph(n, nums, maxDiff):
+    graph = empty graph
+    for i in range(n:
+        for j in range(i+1, n):
+            if abs(nums[i] - nums[j]) <= maxDiff:
+                add edge between i and j to graph
+    return graph
 
-        LOG = 18
-        st = [[0] * LOG for _ in range(n)]
-
-        r = 0
-        for i in range(n):
-            if r < i: r = i
-            while (r + 1 < n and
-                   new_nums[r + 1][1] - new_nums[r][1] <= maxDiff and
-                   new_nums[r + 1][1] - new_nums[i][1] <= maxDiff):
+function bfs(graph, source, target):
+    queue = new queue with source node
+    distance = 0
+    visited = empty set
+    while queue is not empty:
+        node = dequeue from queue
+        if node is target:
+            return distance
