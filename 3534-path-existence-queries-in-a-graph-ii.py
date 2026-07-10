490690835393cbb99b1e0cae1beba66a206adb13
Satyam Kumar <152474324+Satyam810@users.noreply.github.com>
2026-07-11 00:03:59 +0530

    ✅ 3534. Path Existence Queries in a Graph II (Hard)

--- 3534-path-existence-queries-in-a-graph-ii.py
+++ 3534-path-existence-queries-in-a-graph-ii.py
@@ -7,4 +7,10 @@ class Solution(object):
         :type queries: List[List[int]]
         :rtype: List[int]
         """
-        
+        graph = {}
+        for i in range(n):
+            graph[i] = []
+            for j in range(n):
+                if i != j and abs(nums[i] - nums[j]) <= maxDiff:
+                    graph[i].append(j)
+
