fdac074dfb532decf75c6a675c4ac220e30c6985
Satyam Kumar <152474324+Satyam810@users.noreply.github.com>
2026-07-11 00:05:50 +0530

    ✅ 14. Longest Common Prefix (Easy)

---
+++ 14-longest-common-prefix.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        result = ""
+        for chars in zip(*strs):
+            if len(set(chars)) == 1:
+                result += chars[0]
+            else:
+                break
+        return result
