04000f0c162da6cb2a6ddf9daa1cd37e09d929f8
Satyam Kumar <152474324+Satyam810@users.noreply.github.com>
2026-07-10 23:56:38 +0530

    ✅ 3534. Path Existence Queries in a Graph II (Hard)

--- 3534-path-existence-queries-in-a-graph-ii.py
+++ 3534-path-existence-queries-in-a-graph-ii.py
@@ -1,16 +1,16 @@
-from collections import deque, defaultdict
+Function buildGraph(n, nums, maxDiff):
+  Create an empty adjacency list
+  For each node i in the adjacency list:
+    For each node j in the nums array:
+      If abs(nums[i] - nums[j]) <= maxDiff:
+        Add node j to the adjacency list of node i
+  Return the adjacency list
 
-class Solution(object):
-    def shortestPath(self, n, nums, maxDiff, queries):
-        """
-        :type n: int
-        :type nums: List[int]
-        :type maxDiff: int
-        :type queries: List[List[int]]
-        :rtype: List[int]
-        """
-        graph = defaultdict(list)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if abs(nums[i] - nums[j]) <= maxDiff:
-                    graph[i].append(j)
+Function bfs(graph, start, end):
+  Create a queue and enqueue the start node
+  Create a visited set and add the start node
+  While the queue is not empty:
+    Dequeue a node
+    If the node is the end node:
+      Return the distance from the start node to the end node
+    For each neighbor of the node:
