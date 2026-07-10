61e12ed9d4921ab500d1d69149d654821237057e
Satyam Kumar <152474324+Satyam810@users.noreply.github.com>
2026-07-11 00:46:26 +0530

    ✅ Climbing Stairs (Unknown)

--- climbing-stairs.py
+++ climbing-stairs.py
@@ -1,7 +1,8 @@
-class Solution(object):
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        
+class Solution:
+  def climbStairs(self, n: int) -> int:
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n + 1):
+      dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[n]
