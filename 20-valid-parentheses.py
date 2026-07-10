912f77990512f59cb367bb52b9fa9713b05f7ccd
Satyam Kumar <152474324+Satyam810@users.noreply.github.com>
2026-07-10 22:32:11 +0530

    ✅ 20. Valid Parentheses (Easy)

--- 20-valid-parentheses.py
+++ 20-valid-parentheses.py
@@ -1,3 +1,16 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        
+def isValid(s: str) -> bool:
+  # Create a dictionary to map closing parentheses to their corresponding opening parentheses
+  parentheses_map = {')': '(', '}': '{', ']': '['}
+  
+  # Create an empty stack to store the opening parentheses
+  stack = []
+  
+  # Iterate through each character in the input string
+  for char in s:
+    # If the character is an opening parenthesis, push it onto the stack
+    if char in parentheses_map.values():
+      stack.append(char)
+    # If the character is a closing parenthesis
+    elif char in parentheses_map.keys():
+      # Check if the stack is empty or if the top of the stack does not contain the corresponding opening parenthesis
+      if not stack or stack.pop() != parentheses_map[char]:
